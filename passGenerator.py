a7b64944170ee1e36e4b6226618139e9590ce8c5
Bader <104280578+7q1@users.noreply.github.com>
2022-04-24 04:56:17 +0300

    Add files via upload

---
+++ passGenerator.py
@@ -0,0 +1,43 @@
+# Developed & Programmed By 7Q1
+# iG -> @thrudespair
+
+import random
+import time
+import os
+
+userName = os.getlogin()
+numbers = '0123456789'
+abc = 'abcdefghijklmnopqrstuvwxyz'
+all = numbers + abc
+passList = []
+_b = 0
+
+
+# <!---------------------------------------- >
+# Pervent ValueError:
+print("[+] Input Number Only!")
+while True:
+  try:
+    times = int(input('[?] How many passowrds >> '))
+    break
+  except:
+    print("[+] Input Number Only!")
+while True:
+  try:
+    chr = int(input('[?] How many Character >> '))
+    break
+  except:
+    print("[+] Input Number Only!")
+
+# Passwords Generator:
+for i in range(times):
+  password = "".join(random.sample(all, chr))
+  passList.append(password)
+print("Processing . . .")
+for i in passList:
+  i = i + "\n"
+  with open(f'C:\\Users\\{userName}\\Desktop\\passGen.txt', 'a') as f:
+    f.write(i)
+input("[+] Lists Saved AS (passGen.txt) In Desktop.\n\nPress Enter To exit . . .")
+
+# <!---------------------------------------- >
